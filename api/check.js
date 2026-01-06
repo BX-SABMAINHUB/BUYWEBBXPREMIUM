3f8178800e575eac56c6ce5f3fafe7b853484ede
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-06 23:05:50 +0100

    Delete api directory

--- api/check.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  // Genera key de 7 dígitos por día
-  const key = Math.floor(Math.random() * 9000000) + 1000000;
-  
-  res.status(200).send(`{
-    "KEY": ${key}
-  }`);
-}
