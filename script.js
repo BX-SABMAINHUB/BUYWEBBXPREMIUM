29d6554f038e3ebed8f3aba712d938e61823d40d
YT:ALEXGAMING <alexsanchezfollia@gmail.com>
2026-01-07 01:35:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const currencySelect = document.getElementById("currency");
+
 paypal.Buttons({
   createOrder: async function() {
-    const res = await fetch('/api/create-order');
+    const currency = currencySelect.value; // toma la moneda seleccionada
+    const res = await fetch('/api/create-order', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ currency: currency })
+    });
     const data = await res.json();
     return data.id;
   },
